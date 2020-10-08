b222ed68c551ade18fc203105f447c57e6cb6091
Diego <dasalazar90@misena.edu.co>
2020-10-08 11:52:44 -0500

    se cambio el color de la card de informes

--- vistas/modulos/informeVentanas.php
+++ vistas/modulos/informeVentanas.php
@@ -26,14 +26,13 @@
           <div class="col-md-6">
             
             <!-- BAR CHART -->
-            <div class="card card-success">
-              <div class="card-header">
+            <div class="card card">
+              <div class="card-header bg-secondary">
                 <h3 class="card-title">Ventana A</h3>
 
                 <div class="card-tools">
                   <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
                   </button>
-                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
                 </div>
               </div>
               <div class="card-body">
